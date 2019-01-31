d287f87cf4d51e85f63a90e7d7cb13a9574f55b8
Auxxk Vii <auxxkvii@Rashads-Air.fios-router.home>
2019-01-31 17:12:02 -0500

    navigation is installed with no links

---
+++ src/components/Gallary/Gallary.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Gallary extends Component {
+    render() {
+        return (
+            <div>
+                Gallary
+            </div>
+        );
+    }
+}
+
+export default Gallary;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home/Home'
+export { default as Navi } from './Navi/Navi'
+export { default as Gallary } from './Gallary/Gallary'
+export { default as Aboutus } from './Aboutus/Aboutus'
