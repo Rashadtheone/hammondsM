af7dbc17cc53055b6c09c75642a49fbc7d53f71a
Auxxk Vii <auxxkvii@Rashads-Air.fios-router.home>
2019-01-31 19:14:11 -0500

    basic hompage layout

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <Navi />
+      <div className="app-main">
+      <div className= "app-navi">
+      <Navi />
+      </div>
+      <div className="app-body">
           <Route exact path="/hammonds/home" render={() => (<Home />)} />
           <Route exact path="/hammonds/gallary" render={() => (<Gallary />)} />
           <Route exact path="/hammonds/aboutus" render={() => (<Aboutus />)} />
           <Route exact path="/hammonds/services" render={() => (<Services />)} />
           <Route path="/*" render={() => (<Redirect to="/hammonds/home" />)} />
-          <Footer />
+          
+      </div>
+      <div className="app-footer">
+      <Footer />
+      </div>
       </div>
     );
   }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-
+import { Container, Row, Col } from 'reactstrap';
 class Footer extends Component {
     render() {
         return (
-            <div>
-                I am ya footer on errpage
-            </div>
+            <Container>
+                  <Row>
+                    <Col xs="4" md="4">Contact Us</Col>
+                    <Col xs="4" md="4">
+                    <Row>Phone Number</Row>
+                    <Row>Email</Row>
+                    </Col>
+                 </Row>
+            </Container>
         );
     }
 }
