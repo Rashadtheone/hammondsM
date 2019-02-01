a2fcfea73fc0dee44ddd462fa819efd53986d67a
Rashad Hammonds <ShadHam@rashadhammonds.fios-router.home>
2019-02-01 15:48:23 -0500

    services page updated, housses modal to show more on the services offered.

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,28 +1,292 @@
-import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import React, { Component } from "react";
+import {
+  Container,
+  Row,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from "reactstrap";
 
 class Services extends Component {
-    render() {
-        return (
-                <Container>
-                    <Row>
-                        <div className = "services">
-            <div className = 'service'><img src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg" alt="service"/></div>
-            <div className = 'service'><img src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg" alt="service"/></div>
-            <div className = 'service'><img src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg" alt="service"/></div>
-            <div className = 'service'><img src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg" alt="service"/></div>
-            <div className = 'service'><img src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg" alt="service"/></div>
-            <div className = 'service'><img src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg" alt="service"/></div>
-            <div className = 'service'><img src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg" alt="service"/></div>
-            <div className = 'service'><img src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg" alt="service"/></div>
-                     </div> 
-                    </Row>
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
 
+    this.toggle = this.toggle.bind(this);
+  }
 
-                </Container>
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <div className="services">
+            <div className="service">
+              <img
+                src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg"
+                alt="service"
+              />
+              <div className="serviceButton">
+                <Button onClick={this.toggle}>hi</Button>
+                <Modal
+                  isOpen={this.state.modal}
+                  toggle={this.toggle}
+                  className={this.props.className}
+                >
+                  <ModalHeader toggle={this.toggle} charCode="X">
+                    Modal title
+                  </ModalHeader>
+                  <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="primary" onClick={this.toggle}>
+                      Do Something
+                    </Button>{" "}
+                    <Button color="secondary" onClick={this.toggle}>
+                      Cancel
+                    </Button>
+                  </ModalFooter>
+                </Modal>
+              </div>
+            </div>
+            <div className="service">
+              <img
+                src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg"
+                alt="service"
+              />
+              <div className="serviceButton">
+                <Button onClick={this.toggle}>hi</Button>
+                <Modal
+                  isOpen={this.state.modal}
+                  toggle={this.toggle}
+                  className={this.props.className}
+                >
+                  <ModalHeader toggle={this.toggle} charCode="X">
+                    Modal title
+                  </ModalHeader>
+                  <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="primary" onClick={this.toggle}>
+                      Do Something
+                    </Button>{" "}
+                    <Button color="secondary" onClick={this.toggle}>
+                      Cancel
+                    </Button>
+                  </ModalFooter>
+                </Modal>
+              </div>
+            </div>
+            <div className="service">
+              <img
+                src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg"
+                alt="service"
+              />
+              <div className="serviceButton">
+                <Button onClick={this.toggle}>hi</Button>
+                <Modal
+                  isOpen={this.state.modal}
+                  toggle={this.toggle}
+                  className={this.props.className}
+                >
+                  <ModalHeader toggle={this.toggle} charCode="X">
+                    Modal title
+                  </ModalHeader>
+                  <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="primary" onClick={this.toggle}>
+                      Do Something
+                    </Button>{" "}
+                    <Button color="secondary" onClick={this.toggle}>
+                      Cancel
+                    </Button>
+                  </ModalFooter>
+                </Modal>
+              </div>
+            </div>
+            <div className="service">
+              <img
+                src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg"
+                alt="service"
+              />
+              <div className="serviceButton">
+                <Button onClick={this.toggle}>hi</Button>
+                <Modal
+                  isOpen={this.state.modal}
+                  toggle={this.toggle}
+                  className={this.props.className}
+                >
+                  <ModalHeader toggle={this.toggle} charCode="X">
+                    Modal title
+                  </ModalHeader>
+                  <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="primary" onClick={this.toggle}>
+                      Do Something
+                    </Button>{" "}
+                    <Button color="secondary" onClick={this.toggle}>
+                      Cancel
+                    </Button>
+                  </ModalFooter>
+                </Modal>
+              </div>
+            </div>
+            <div className="service">
+              <img
+                src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg"
+                alt="service"
+              />
+              <div className="serviceButton">
+                <Button onClick={this.toggle}>hi</Button>
+                <Modal
+                  isOpen={this.state.modal}
+                  toggle={this.toggle}
+                  className={this.props.className}
+                >
+                  <ModalHeader toggle={this.toggle} charCode="X">
+                    Modal title
+                  </ModalHeader>
+                  <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="primary" onClick={this.toggle}>
+                      Do Something
+                    </Button>{" "}
+                    <Button color="secondary" onClick={this.toggle}>
+                      Cancel
+                    </Button>
+                  </ModalFooter>
+                </Modal>
+              </div>
+            </div>
+            <div className="service">
+              <img
+                src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg"
+                alt="service"
+              />
+              <div className="serviceButton">
+                <Button onClick={this.toggle}>hi</Button>
+                <Modal
+                  isOpen={this.state.modal}
+                  toggle={this.toggle}
+                  className={this.props.className}
+                >
+                  <ModalHeader toggle={this.toggle} charCode="X">
+                    Modal title
+                  </ModalHeader>
+                  <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="primary" onClick={this.toggle}>
+                      Do Something
+                    </Button>{" "}
+                    <Button color="secondary" onClick={this.toggle}>
+                      Cancel
+                    </Button>
+                  </ModalFooter>
+                </Modal>
+              </div>
+            </div>
+            <div className="service">
+              <img
+                src="https://www.gaskinsbennett.com/wp-content/uploads/2017/06/placeholder-500x500.jpg"
+                alt="service"
+              />
+              <div className="serviceButton">
+                <Button onClick={this.toggle}>hi</Button>
+                <Modal
+                  isOpen={this.state.modal}
+                  toggle={this.toggle}
+                  className={this.props.className}
+                >
+                  <ModalHeader toggle={this.toggle} charCode="X">
+                    Modal title
+                  </ModalHeader>
+                  <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="primary" onClick={this.toggle}>
+                      Do Something
+                    </Button>{" "}
+                    <Button color="secondary" onClick={this.toggle}>
+                      Cancel
+                    </Button>
+                  </ModalFooter>
+                </Modal>
+              </div>
+            </div>
            
-        );
-    }
+          </div>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default Services;
